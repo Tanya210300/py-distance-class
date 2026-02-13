c6786ec45aa0c2f3a80112747092a3614ff980de
Tanya <tanya210300@gmail.com>
2026-02-13 14:41:03 +0200

    fix

--- app/main.py
+++ app/main.py
@@ -4,17 +4,17 @@ class Distance:
 
     def __str__(self) -> str:
         return f"Distance: {self.km}: kilometers. "
-    
+
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
-    
+
     def __add__(self, other: object) -> "Distance":
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         if isinstance(other, (int, float)):
             return Distance(self.km + other)
         return NotImplemented
-    
+
     def __iadd__(self, other: object) -> "Distance":
         if isinstance(other, Distance):
             self.km += other.km
@@ -23,38 +23,38 @@ class Distance:
             self.km += other
             return self
         return NotImplemented
-    
+
     def __mul__(self, other: object) -> "Distance":
         if isinstance(other, (int, float)):
             return Distance(self.km * other)
         return NotImplemented
-    
-    def __truediv__(self, other) -> "Distance":
+
+    def __truediv__(self, other: object) -> "Distance":
         if isinstance(other, (int, float)):
             return Distance(self.km / other)
         return NotImplemented
-    
-    def __eq__(self, other) -> bool:
+
+    def __eq__(self, other: object) -> bool:
         if isinstance(other, Distance):
             return self.km == other.km
         return NotImplemented
 
-    def __lt__(self, other) -> bool:
+    def __lt__(self, other: object) -> bool:
         if isinstance(other, Distance):
             return self.km < other.km
         return NotImplemented
 
-    def __le__(self, other) -> bool:
+    def __le__(self, other: object) -> bool:
         if isinstance(other, Distance):
             return self.km <= other.km
         return NotImplemented
 
-    def __gt__(self, other) -> bool:
+    def __gt__(self, other: object) -> bool:
         if isinstance(other, Distance):
             return self.km > other.km
         return NotImplemented
 
-    def __ge__(self, other) -> bool:
+    def __ge__(self, other: object) -> bool:
         if isinstance(other, Distance):
             return self.km >= other.km
         return NotImplemented
